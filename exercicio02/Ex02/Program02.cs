9331c3919e5478cda067a83536ffd6b73629aab7
AlefSillva <alef.santos@al.infnet.edu.br>
2025-03-31 18:32:54 -0300

    renomeação do arquivo program para program02

---
+++ exercicio02/Ex02/Program02.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex02
+{
+    class Program02
+    {
+        public static void Main(string[] args)
+        {
+            string meuNome = "Alef Silva";
+
+            string nomeCifrado = cifradorDeNome(meuNome);
+            Console.WriteLine(nomeCifrado);
+
+
+        }
+
+        static string cifradorDeNome(string nome)
+        {
+            char[] nomeArray = nome.ToCharArray();
+
+            for (int i = 0; i < nomeArray.Length; i++)
+            {
+                if (char.IsLetter(nomeArray[i]))
+                {
+                    char letraMinuscula = char.IsUpper(nomeArray[i]) ? 'A' : 'a';
+                    nomeArray[i] = (char)(letraMinuscula + (nomeArray[i] - letraMinuscula + 2) % 26);
+                }
+            }
+
+            return new string(nomeArray);
+        }
+    }
+}
