5d5d73ea799b0a2a7ad27f12eea512e221657d8c
AlefSillva <alef.santos@al.infnet.edu.br>
2025-03-31 18:51:58 -0300

    criação do ex01

---
+++ exercicio01/Ex01/Program01.cs
@@ -0,0 +1,8 @@
+﻿class Program01
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, meu nome é Alef!");
+        Console.WriteLine("Nasci em 15/02/1995 e estou aprendendo C#!");
+    }
+}
